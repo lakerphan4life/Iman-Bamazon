75f9968c4fea97dd5f11aeeee9b16137feaeb261
Iman <imanhamidi91@gmail.com>
2017-04-18 17:37:47 -0700

    done

---
+++ bamazon.js
@@ -0,0 +1,66 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var prompt = require('prompt');
+
+var connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '',
+		database: 'bamazon_db',
+});
+
+connection.connect(function(err) {
+		if(err) throw err;
+		start();
+});
+
+
+var start = function() {
+		connection.query('SELECT * FROM bamazonProducts', function(err, res) {
+				for (var i=0; i<res.length; i++) {
+						if (res[i].stock_quantity > 0) {
+							console.log('ID: ' + res[i].item_id + ' | ' + 'Product: ' + res[i].product_name + ' | ' + 'Price: ' + '$' + res[i].price);
+						}
+
+				}
+
+				shopProduct();
+		})
+};
+
+
+var shopProduct = function() {
+		prompt.get([{
+				name: 'product_id',
+				type: 'input',
+				message: 'Type in the product_id of the product that you would like to purchase',
+				required: true
+		},{
+				name: 'product_quantity',
+				type: 'input',
+				message: 'What is your desired product quantity that you would like to purchase?',
+		}], function(err, res) {
+			console.log(res);
+				var product_id = res.product_id.toUpperCase();
+				var product_quantity = res.product_quantity.toUpperCase();
+
+			connection.query('SELECT * FROM bamazonProducts WHERE ?', {item_id: product_id}, function(err, res) {
+							if (err) throw err;
+							if(res[0].stock_quantity >= product_quantity) {
+								var stock = res[0].stock_quantity;
+								var sales = res[0].product_sales;
+								var productCost = product_quantity * res[0].price;
+								connection.query('UPDATE bamazonProducts SET ? WHERE ?', [{stock_quantity: stock - product_quantity}, {item_id: purchase_id}])
+									console.log('Product Cost = ' + '$' + productCost);
+									var newRevenue = productCost + sales;
+									connection.query("UPDATE products SET ? WHERE ?", [{product_sales: newRevenue}, {item_id: product_id}])
+							} else {
+								console.log('Insufficient Quantity, try ordering a smaller amount')
+							}
+					});
+			})
+		    
+}
+
+start();
